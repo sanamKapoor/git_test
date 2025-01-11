7ec3922bf3a6e6ed7b5e3a2592f2653c702d85ed
sanam-kapoor-md <sanam@alturalabz.com>
2025-01-11 20:30:59 +0530

    added husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 function App() {    
+
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <h1>Hello World</h1>
+      <button onClick={() => setCount(count + 1)}>
+        count is {count}
+      </button>
     </>
   )
 }
