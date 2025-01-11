cfa483423ffbc6532dcb39267e7f3b0ffd32820f
sanam-kapoor-md <sanam@alturalabz.com>
2025-01-11 20:47:20 +0530

    added husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-function App() {    
+function App() {
+    const [count, setCount] = useState(0);
 
-  const [count, setCount] = useState(0)
+    console.log('test');
 
-  return (
-    <>
-      <h1>Hello World</h1>
-      <button onClick={() => setCount(count + 1)}>
-        count is {count}
-      </button>
-    </>
-  )
+    const unused = 'this will cause a lint error';
+
+    return (
+        <>
+            <h1>Hello World</h1>
+            <button onClick={() => setCount(count + 1)}>
+                count is {count}
+            </button>
+        </>
+    );
 }
 
-export default App
+export default App;
